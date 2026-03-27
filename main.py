fe2cca0452318e2a7a2a032fdc26e735526245aa
duck-irl <94648379+duck-irl@users.noreply.github.com>
2026-03-27 14:30:43 -0400

    Project Template

---
+++ main.py
@@ -0,0 +1,22 @@
+class Station:
+    def __init__(self):
+        pass
+
+    def add_train():
+        pass
+
+    def check_times():
+        pass
+
+class Train:
+    def __init__(self):
+        pass
+
+def get_stations():
+    pass
+
+def refresh_data():
+    pass
+
+if __name__ == "__main__":
+    all_stations = []
