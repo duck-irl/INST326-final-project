043551ec0ea78315d9e137bc48d0a9c861f6d801
liannabana <lianna.buszkiewicz@gmail.com>
2026-04-16 22:50:49 -0400

    Planned out unit tests for the add_train() and check_times() methods

---
+++ test_main.py
@@ -0,0 +1,20 @@
+
+def test_add_train():
+    #checks if a train is properly added to the station's trains list
+    pass
+
+def test_check_times_arriving():
+    #checks if a train with estimated_time "ARR" returns an arriving message
+    pass
+
+def test_check_times_boarding():
+    #checks if a train with estimated_time "BRD" returns a boarding message
+    pass
+
+def test_check_times_skips_non_passenger():
+    #checks if a train with estimated_time "---" is skipped and returns an empty line
+    pass
+
+def test_check_times_minutes():
+    #checks if a train with a numeric estimated_time returns the correct minutes 
+    pass 
